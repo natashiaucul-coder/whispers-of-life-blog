6b72c83df131fe4e308f119bac49a332b648486c
natashiaucul-coder <natashia.ucul@gmail.com>
2025-10-03 17:58:23 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  content: ['./pages/**/*.{js,jsx}', './components/**/*.{js,jsx}', './posts/**/*.md'],
+  theme: {
+    extend: {},
+  },
+  plugins: [require('@tailwindcss/typography')],
+}
